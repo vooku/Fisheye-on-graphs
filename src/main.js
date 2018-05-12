54c597b950d92e6374f344ff7dad7eb91d7d79ee
Vadim Petrov <vadim.vooku.petrov@gmail.com>
2018-05-12 18:38:50 +0200

    Transform to screen coords

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
 var nnodes = 200;
 var nedges = 1000;
 
-function parseNodes(data, options, maxx, maxy) {
+function parseNodes(data, options, canvasWidth, canvasHeight) {
     var nodes = new vis.DataSet(options);
 
-    for (i in data["nodes"]) {
-        var node = data["nodes"][i];
-        var item = {
-            id: node["_id"],
-            x: node["_attributes"]["x"],
-            y: node["_attributes"]["y"],
-            label: node["_attributes"]["tooltip"].substring(0,3)
-        };
+    for (const node of data["nodes"]) {
+        var x = node["_attributes"]["x"];
+        var y = node["_attributes"]["y"];
 
-        nodes.add(item);
+        nodes.add({
+            id: node["_id"],
+            x: x,
+            y: y,
+            origin: {
+                x: x,
+                y: y
+            },
+            label: node["_attributes"]["tooltip"].substring(0,3)
+        });
     }
+
+    // generate screen coords
+    var xMax = nodes.max("x").x;
+    var xMin = nodes.min("x").x;
+    var yMax = nodes.max("y").y;
+    var yMin = nodes.min("y").y;
+    var w = xMax - xMin;
+    var h = yMax - yMin;
+    var ratio = w / h;
+
+    nodes.forEach(function(node) {
+        var x = (-xMin + node.x) / w * canvasWidth;
+        var y = (-yMin + node.y) / h * canvasWidth / ratio;
+
+        nodes.update({id: node.id, x: x, y: y, origin: {x: x, y: y}});
+    });
 
     return nodes;
 }
@@ -22,15 +42,12 @@ function parseNodes(data, options, maxx, maxy) {
 function parseEdges(data, options) {
     var edges = new vis.DataSet(options);
 
-    for (i in data["edges"]) {
-        var edge = data["edges"][i];
-        var item = {
+    for (const edge of data["edges"]) {
+        edges.add({
             id: edge["_id"],
             from: edge["_source"],
             to: edge["_target"]
-        };
-
-        edges.add(item);
+        });
     }
 
     return edges;
@@ -38,7 +55,7 @@ function parseEdges(data, options) {
 
 var width = document.getElementById('visualization').getBoundingClientRect().width;
 var height = document.getElementById('visualization').getBoundingClientRect().height;
-console.log(width + ", " + height);
+
 var options = {};
 var nodes = parseNodes(data, options, width, height);
 var edges = parseEdges(data, options);
@@ -55,7 +72,7 @@ options = {
     autoResize: true,
     clickToUse: true,
     nodes: {
-        shape: 'dot',
+        shape: 'circle',
         fixed: {
             x: true,
             y: true
